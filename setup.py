bab3a48d3994d17e55c026d4f5a97fd69d4941a0
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-06-16 20:09:19 +0530

    updated folder and file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="siddhanath-tiwari",
+    author_email="pratimatiwari8299@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
